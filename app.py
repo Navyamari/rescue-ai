2b47ebd711905296ef667222b30538d8ea355de8
Navya Mari <navyakrishnaa.05@gmail.com>
2024-11-30 14:48:52 -0500

    Update app.py for production

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS  # Import CORS to allow cross-origin requests
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
@@ -21,4 +22,6 @@ def analyze():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Use the PORT environment variable for the port number in production (Heroku)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if not set
+    app.run(host="0.0.0.0", port=port, debug=False)  # Disable debug mode in production
